5387b93469b5462c5730cca661c4a2681aa904b6
gaurav13037@iiitd.ac.in <gaurav13037@iiitd.ac.in>
2022-08-24 11:23:01 +0000

    c

--- notebooks/DataQuicks/DataQuicks_Logger.py
+++ notebooks/DataQuicks/DataQuicks_Logger.py
@@ -29,18 +29,20 @@ class Logger:
        
     def DataQuicks_Logger(self,Job_Name, Task_Name, Start = True, End = False, Status = "Running"):
         Start_time, End_time = None, None
+        Host_Id = context['tags']['hostName']
         if Start:
             Start_time = self.start_time
         if End:
             End_time = datetime.now()
         spark.sql("""
         insert into dataquicks.logs (Job_Name, Task_Name, Start_Time, End_Time, Status) 
-        values ('{Job_Name}','{Task_Name}','{Start_time}','{End_time}','{Status}')
-        """.format(Job_Name = Job_Name,
-                  Task_Name = Task_Name,
-                  Start_time = Start_time,
-                  End_time = End_time,
-                  Status = Status))
+        values ('{Host_Id}','{Job_Name}','{Task_Name}','{Start_time}','{End_time}','{Status}')
+        """.format(Host_Id = Host_Id,
+                   Job_Name = Job_Name,
+                   Task_Name = Task_Name,
+                   Start_time = Start_time,
+                   End_time = End_time,
+                   Status = Status))
         return None
 
 # COMMAND ----------
@@ -61,6 +63,7 @@ class Logger:
 # %sql
 # Drop Table if exists dataquicks.logs; 
 # Create Table dataquicks.logs (
+#   Host_Id varchar(200),
 #   Job_Name varchar(100),
 #   Task_Name varchar(100),
 #   Start_Time varchar(100),
